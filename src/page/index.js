c83f0c6df3c3b8f67de804535fc90176385450e1
judandan <judandan0608@163.com>
2017-07-14 21:46:01 +0800

    update

--- src/page/index.js
+++ src/page/index.js
@@ -6,12 +6,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import Font from '../components/fontLight/index';
-// import FontTest from '../components/fontTest/index';
-import Wood from '../components/wood/index';
-
-import { FontLight } from "fdcomponent";
-
+import { FontLight, Wood } from "fdcomponent";
 import "./index.css";
 
 class App extends React.Component {
@@ -26,14 +21,14 @@ class App extends React.Component {
 		return (
 			<div className="container">
 				<div className="fd-title">
-					<FontLight test="WebSiteDA" color={["#6688ef", "#71ef30"]} />
+					<FontLight test="dict-life" color={["#6688ef", "#71ef30"]} />
 				</div>
-				{/*<Wood />*/}
-				<div className="fd-flow">
+				{/* <Wood /> */}
+				{/* <div className="fd-flow">
 					<span className="flow-comm flow-c">css</span>
 					<span className="flow-comm flow-a">abc</span>
 					<span className="flow-comm flow-x">xml</span>
-				</div>
+				</div> */}
 				
 			</div>
 		)
